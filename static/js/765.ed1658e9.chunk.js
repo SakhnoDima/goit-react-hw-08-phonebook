e9007277e5eaa1631e0e2e14cb362e75493fa29e
SakhnoDima <134371251+SakhnoDima@users.noreply.github.com>
2023-09-09 09:44:16 +0000

    Deploying to gh-pages from @ SakhnoDima/goit-react-hw-08-phonebook@8812c3bcaaff663ae40a17bacbe4e1a9ffd56c74 🚀

---
+++ static/js/765.ed1658e9.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[765],{9765:function(e,o,n){n.r(o);n(2791);var t=n(3329);o.default=function(){return(0,t.jsx)("div",{children:"Home"})}}}]);
+//# sourceMappingURL=765.ed1658e9.chunk.js.map
